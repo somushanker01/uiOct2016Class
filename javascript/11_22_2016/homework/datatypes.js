a64d1ca8db647dc5f14540455095a36e3531e83c
somushanker01 <somushanker01@gmail.com>
2016-11-22 13:45:22 -0800

    done with todays homework

---
+++ javascript/11_22_2016/homework/datatypes.js
@@ -0,0 +1,43 @@
+/**
+ * Created by somu on 11/22/2016.
+ */
+/*primitive data types*/
+/*a). number*/
+
+var a = 23;
+var c = 101.0310;
+
+console.log(a);
+console.log(c);
+
+/*b). string*/
+
+var firstName = 'somushanker';
+var lastName = 'vattamvar';
+
+console.log(firstName);
+console.log(lastName);
+
+/*c). boolean*/
+
+var fname = true;
+var lname = false;
+
+console.log(fname);
+console.log(lname);
+
+/*d). null*/
+
+var first = null;
+
+console.log(first);
+
+/*e). undefined*/
+
+var last ;
+
+console.log(last);
+
+
+
+
