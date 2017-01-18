2b3c3544cde1b68539b43f4c4c21aa6f83f78117
somushanker01 <somushanker01@gmail.com>
2017-01-18 11:24:10 -0800

    functions

---
+++ somushanker/01_17_2017/basics.js
@@ -0,0 +1,18 @@
+/**
+ * Created by somu on 1/17/2017.
+ */
+function addition() {
+    var x = 1;
+    x++;
+    return x;
+}
+console.log(addition());
+var test = addition();
+console.log(test);
+var y = function (a,b) {
+    var sum = a+b;
+    return sum;
+
+
+};
+console.log(y(2,4));
